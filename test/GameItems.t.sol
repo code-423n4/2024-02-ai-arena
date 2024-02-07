9c9367eaa3969d402d3e639ade7e8d920b8ad3b3
Sonny <65030730+SonnyCastro@users.noreply.github.com>
2024-02-07 12:02:48 -0500

    added test stdError import

--- test/GameItems.t.sol
+++ test/GameItems.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console, stdError} from "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
 import {GameItems} from "../src/GameItems.sol";
 import {Neuron} from "../src/Neuron.sol";
